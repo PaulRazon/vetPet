9b294a324c12d7a9374c06d9d8bfbf8274c1158e
Edwin Ramón Montoya Valdez <edramontoyava@ittepic.edu.mx>
2023-12-02 13:43:25 -0700

    login & auth_guard

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { LoginPageRoutingModule } from './login-routing.module';
+import { LoginPage } from './login.page';
+import { ToolbarModule } from '../toolbar/toolbar.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule,
+    ToolbarModule
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}
