f06291f3bab9bf34b6440f1f16eb16a446e98a1a
Valeria <vadigutierrezde@ittepic.edu.mx>
2023-12-05 22:39:58 -0700

    agregar productos sin imagen

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -11,14 +11,64 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page {
+  public products: Product[] = [];
+  public productsFounds: Product[] = [];
+  public filter = [
+    "Juguetes y Accesorios",
+    "Ropa",
+    "Limpieza",
+    "Alimento",
+  ];
+
+  public colors = [
+    {
+      type: "Juguetes y Accesorios",
+      color: "primary"
+    },
+    {
+      type: "Ropa",
+      color: "secondary"
+    },
+    {
+      type: "Limpieza",
+      color: "warning"
+    },
+    {
+      type: "Alimento",
+      color: "danger"
+    }
+  ];
 
   constructor(
     private router: Router, private productService: ProductService, private authService: AuthService,
-    private alertController: AlertController
-  ) { }
+    private alertController: AlertController) {
+      this.productService.getProducts().subscribe((products: Product[]) => {
+        this.products = products;
+        this.productsFounds = this.products;
+      });
+  
+     }
 
   openProductAddPage() {
     this.router.navigate(['/add-product']); // Asume que la ruta 'product-add' existe para añadir productos.
   }
+
+  public filterProducts(): void {
+    console.log(this.filter);
+    this.productsFounds = this.products.filter(
+      item => {
+        return this.filter.includes(item.type);
+      }
+    );
+  }
+
+  public getColor(type: string): string {
+    const itemFound = this.colors.find((element) => {
+      return element.type === type;
+    });
+    let color = itemFound && itemFound.color ? itemFound.color : "";
+    return color;
+  }
+
 
 }
